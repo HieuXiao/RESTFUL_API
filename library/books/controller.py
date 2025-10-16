5c2c90b65d3172d4de180065697bf150d177737d
MrHieu <hieunm.se@gmail.com>
2025-10-16 16:12:23 +0700

    feat: Completed CRUD (r-u-d)

--- library/books/controller.py
+++ library/books/controller.py
@@ -1,14 +1,32 @@
 from flask import Blueprint
 
-from .services import add_book_service
+from .services import (add_book_service, get_book_by_id_service,
+                       get_all_books_service, update_book_by_id_service,
+                       delete_book_by_id_service)
 
 books = Blueprint('books', __name__)
-
-@books.route('/get-all-books')
-def get_all_books():
-    return " Book:'How To Print 'HELLO WORLD' Text By Python' "
 
 # METHOD: Add a new book
 @books.route('/book-management/book', methods=['POST'])
 def add_book():
     return add_book_service()
+
+#METHOD: Get book by id
+@books.route('/book-management/book/<int:id>', methods=['GET'])
+def get_book_by_id(id):
+    return get_book_by_id_service(id)
+
+#METHOD: Get all book
+@books.route('/book-management/books', methods=['GET'])
+def get_all_books():
+    return get_all_books_service()
+
+#METHOD: Update book
+@books.route('/book-management/book/<int:id>', methods=['PUT'])
+def update_book_by_id(id):
+    return update_book_by_id_service(id)
+
+#METHOD: Update book
+@books.route('/book-management/book/<int:id>', methods=['DELETE'])
+def delete_book_by_id(id):
+    return delete_book_by_id_service(id)
--- library/books/services.py
+++ library/books/services.py
@@ -1,21 +1,73 @@
 from library.extension import db
 from library.library_ma import BookSchema
 from library.model import Books
-from flask import request
+from flask import request, jsonify
 
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
 
+
 def add_book_service():
-    name = request.json['name']
-    page_count = request.json['page_count']
-    author_id = request.json['author_id']
-    category_id = request.json['category_id']
-    try:
-        new_book = Books(name, page_count, author_id, category_id)
-        db.session.add(new_book)
-        db.session.commit()
-        return 'Add new book successfully', 201
-    except IndentationError:
-        db.session.rollback()
-        return "Can't add new book", 400
+    data = request.json
+    if (data and ('name' in data) and ('page_count' in data)
+            and ('author_id' in data) and ('category_id' in data)):
+        name = data['name']
+        page_count = data['page_count']
+        author_id = data['author_id']
+        category_id = data['category_id']
+        try:
+            new_book = Books(name, page_count, author_id, category_id)
+            db.session.add(new_book)
+            db.session.commit()
+            return 'Add new book successfully', 201
+        except IndentationError:
+            db.session.rollback()
+            return "Can't add new book", 400
+    else:
+        return 'Request error', 400
+
+
+def get_book_by_id_service(id):
+    book = Books.query.get(id)
+    if book:
+        return book_schema.jsonify(book)
+    else:
+        return 'Book not found', 404
+
+
+def get_all_books_service():
+    books = Books.query.all()
+    if books:
+        return books_schema.jsonify(books)
+    else:
+        return 'No book found', 404
+
+
+def update_book_by_id_service(id):
+    book = Books.query.get(id)
+    data = request.json
+    if book:
+        if data and 'page_count' in data:
+            try:
+                book.page_count = data['page_count']
+                db.session.commit()
+                return 'Update book successfully', 200
+            except IndentationError:
+                db.session.rollback()
+                return "Can't update book", 400
+    else:
+        return 'Book not found', 404
+
+
+def delete_book_by_id_service(id):
+    book = Books.query.get(id)
+    if book:
+        try:
+            db.session.delete(book)
+            db.session.commit()
+            return 'Delete book successfully', 200
+        except IndentationError:
+            db.session.rollback()
+            return "Can't delete book", 400
+    else:
+        return 'Book not found', 404
